563f08d70e1a82a600b86ce7b2bb532aa7d971f0
leeyanleryan <leeyanleryan.21@gmail.com>
2024-07-27 16:48:35 +0800

    binary heap starting

---
+++ BinaryHeap/BinaryHeap.py
@@ -0,0 +1,13 @@
+class BinaryHeap:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def Insert():
+        pass
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
